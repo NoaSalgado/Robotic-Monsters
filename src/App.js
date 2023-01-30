b787914b9aa921e80c12e5ab6e8546dddee1961e
Noa <noasalgadodev@gmail.com>
2023-01-30 12:19:06 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://random-data-api.com/api/v2/users?size=20')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => ({ monsters: users })));
+  }
+
+  render() {
+    return <div className='App'></div>;
+  }
+}
+
+export default App;
