24e094307e5c0d470465edfd1ad11437f4d2bdc5
Noa <noasalgadodev@gmail.com>
2023-01-30 13:14:27 +0100

    Add render info functionality through CardList and Card components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch('https://random-data-api.com/api/v2/users?size=25')
+    fetch('https://random-data-api.com/api/v2/users?size=20')
       .then((response) => response.json())
       .then((users) => this.setState(() => ({ monsters: users })));
   }
 
   render() {
-    return <div className='App'></div>;
+    const { monsters } = this.state;
+
+    return (
+      <div className='App'>
+        <CardList monsters={monsters}></CardList>
+      </div>
+    );
   }
 }
 
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    const { id, first_name, last_name, email } = this.props.monster;
+    return (
+      <div className='card'>
+        <img
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+          alt={first_name}
+        />
+        <h2>
+          {first_name} {last_name}
+        </h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
